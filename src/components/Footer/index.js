0081602b683f52865d07fc69345663c6e5ff00ec
cdis001 <shopingid@naver.com>
2022-06-26 00:04:16 +0900

    update. header & footer  - header css 수정  - footer js, css 추가

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <div className="joyedu_footer_container">
+      <p>
+        조이학습심리상담 소윤미 경기 고양시 일산동구 일산로 228 강서빌딩 202호
+        사업자등록번호 : 547-68-00324 전화번호 : 031-901-6563
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,11 +3,31 @@ import ReactDOM from "react-dom/client";
 
 import "./App.css";
 import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import Footer from "../../components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <div className="introduce_joyEdu">
+        <h1>JOY EDU</h1>
+        <div className="line" />
+        <pre>
+          '학습심리상담센터'와 '학습코칭학원'의 콜라보! 상담과 교육이 만나
+          "행복한 마음 즐거운 공부" 의 솔루션을 제공합니다.
+        </pre>
+        <div className="curriculum_joyEdu">
+          <h2>교육 과정</h2>
+          <div className="curriculum_box_joyEdu">
+            <div className="curriculum_content_joyEdu"></div>
+            <div className="curriculum_content_joyEdu"></div>
+            <div className="curriculum_content_joyEdu"></div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
